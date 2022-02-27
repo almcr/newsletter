9b3f8b3501c05688e5692d54a4f2dcea0a0abebb
almcr <alm.dev2@gmail.com>
2022-02-27 20:41:13 +0100

    first route health_check using actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-  println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, HttpRequest};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+  HttpResponse::Ok().body("hello world!")
+}
+
+#[get("/health_check")]
+async fn health_check() -> impl Responder {
+  HttpResponse::Ok()
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+  HttpServer::new(|| {
+    App::new()
+      .service(hello)
+      .service(health_check)
+  })
+  .bind(("127.0.0.1", 8080))?
+  .run()
+  .await
 }
