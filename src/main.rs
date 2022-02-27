8fefee3f0e28e0fd53529001d4a3e1199e91b3dc
almcr <alm.dev2@gmail.com>
2022-02-27 22:17:28 +0100

    polishing and refacto of health_check integration test

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,6 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
-
-#[get("/")]
-async fn hello() -> impl Responder {
-  HttpResponse::Ok().body("hello world!")
-}
-
-#[get("/health_check")]
-async fn health_check() -> impl Responder {
-  HttpResponse::Ok()
-}
+use newsletter::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-  HttpServer::new(|| {
-    App::new()
-      .service(hello)
-      .service(health_check)
-  })
-  .bind(("127.0.0.1", 8080))?
-  .run()
-  .await
+  run()?.await
 }
